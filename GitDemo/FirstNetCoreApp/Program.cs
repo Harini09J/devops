c06e6a91af5669860a9746e13435dd7f5dc478a2
99002541 <harini.j@ltts.com>
2020-10-20 14:41:05 +0530

    My First Commit

---
+++ GitDemo/FirstNetCoreApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+    }
+}
